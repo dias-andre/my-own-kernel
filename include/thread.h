215a5d9472f9a43824663911ef854dfba51cd04b
https-dre <diaso.andre@outlook.com>
2025-06-30 21:18:27 -0300

    fix tss descriptor scope

--- include/thread.h
+++ include/thread.h
@@ -3,23 +3,17 @@
 
 #include <stdint.h>
 
-typedef struct {
-    uint32_t reserved0;
-    uint64_t rsp0;
-    uint64_t rsp1;
-    uint64_t rsp2;
-    uint64_t reserved1;
-    uint64_t ist1;
-    uint64_t ist2;
-    uint64_t ist3;
-    uint64_t ist4;
-    uint64_t ist5;
-    uint64_t ist6;
-    uint64_t ist7;
-    uint64_t reserved2;
-    uint16_t reserved3;
-    uint16_t iomap_base;
-} __attribute__((packed)) tss_t;
+typedef struct
+{
+  uint64_t rsp0; // Stack para modo kernel (RPL=0)
+  uint64_t rsp1; // Reservado
+  uint64_t rsp2; // Reservado
+  uint64_t reserved1;
+  uint64_t ist[7]; // Pilhas para interrupções (IST)
+  uint64_t reserved2;
+  uint16_t reserved3;
+  uint16_t iomap_base; // Offset do I/O bitmap (ou sizeof(tss_t))
+} __attribute__((packed, aligned(16))) tss_t;
 
 void tss_init(uint64_t rsp0);
 void load_tss();
