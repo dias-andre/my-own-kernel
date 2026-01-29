2d40d5c92db9ebcfb7401c565fb113004798e2ff
https-dre <diaso.andre@outlook.com>
2026-01-29 17:33:52 -0300

    test(memory): add heap alignment tests

--- kernel/mm/index.zig
+++ kernel/mm/index.zig
@@ -12,6 +12,32 @@ pub fn init(mb_info: *mb.MultibootInfo, kernel_end_addr: usize) void {
     pmm.init(mb_info, kernel_end_addr);
     vmm.init();
     init_kernel_heap();
+    const memory = kheap.alloc(64) orelse {
+        vga.printError("[TEST] Failed to allocate kernel heap.\n");
+        while(true) asm volatile("hlt");
+    };
+    const addr1 = @intFromPtr(memory);
+    if(addr1 % 16 != 0) {
+        vga.printError("[TEST] Failed to allocate memory aligned to 16 bytes");
+        while(true) asm volatile("hlt");
+    }
+    memory[0] = 'c';
+    
+    const memoryB = kheap.alloc(16) orelse {
+        vga.print("[TEST] Failed to allocate kernel heap.\n");
+        while(true) asm volatile("hlt");
+    };
+
+    const addr2 = @intFromPtr(memoryB);
+    if(addr2 % 16 != 0) {
+        vga.printError("[TEST] Failed to allocate memory aligned to 16 bytes");
+        while(true) asm volatile("hlt");
+    }
+
+    memoryB[1] = 'b';
+    kheap.free(memory);
+    kheap.free(memoryB);
+    vga.print("=> Memory tests passed!\n");
     vga.print("[MM] Memory Management initialized successfully.\n");
 }
 
@@ -24,8 +50,9 @@ fn init_kernel_heap() void {
     kheap = heap.Heap.init(HEAP_START, HEAP_INITIAL_SIZE, pml4_phys, vmm.PAGE_PRESENT | vmm.PAGE_RW);
 }
 
-pub fn kmalloc(space: usize) ![]u8 {
-    return kheap.alloc(space);
+pub fn kmalloc(space: usize) ?[]u8 {
+    const allocated = kheap.alloc(space) orelse return null;
+    return allocated[0..space];
 }
 
 pub fn kfree(address: [*]u8) void {
@@ -33,8 +60,17 @@ pub fn kfree(address: [*]u8) void {
 }
 
 pub fn create(comptime T: type) ?*T {
-    const slice = kheap.alloc(@sizeOf(T));
-    const ptr: *T = @ptrCast(@alignCast(slice.ptr));
-    @memset(slice, 0);
+    const raw_ptr = kheap.alloc(@sizeOf(T)) orelse return null;
+    const addr = @intFromPtr(raw_ptr);
+    
+    if (addr % @alignOf(T) != 0) {
+        vga.printError("Memória não alinhada ao alocar struct");
+        while(true) asm volatile("hlt");
+        return null;
+    }
+
+    const ptr: *T = @ptrCast(@alignCast(raw_ptr));
+    const temp_slice = raw_ptr[0..@sizeOf(T)];
+    @memset(temp_slice, 0);
     return ptr;
 }
