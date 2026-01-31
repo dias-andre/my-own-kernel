0d3be9fe6879e2bdaa81da70e741565c2de65609
https-dre <diaso.andre@outlook.com>
2026-01-31 02:54:35 -0300

    feat(process): introduces process fork function

--- kernel/proc/process.zig
+++ kernel/proc/process.zig
@@ -1,13 +1,37 @@
+const manager = @import("manager.zig");
+const mem = @import("../mm/index.zig");
+
 pub const Process = struct {
     id: usize,
     name: []const u8,
     ref_count: usize,
     page_directory: usize,
-    next: ?*Process,
 
     parent: ?*Process,
     first_child: ?*Process,
     next_sibling: ?*Process,
-    
 };
 
+pub fn fork(parent: *Process) !usize {
+    const pml4_clone = try mem.clone_pml4(parent.page_directory);
+    
+    const new_process = try manager.create_process(parent.name, pml4_clone);
+    new_process.parent = parent;
+
+    if(!parent.first_child)  {
+        parent.first_child = new_process;
+        return new_process.id;
+    }
+
+    var last_sibling = get_last_sibling(parent);
+    last_sibling.next_sibling = new_process;
+    return new_process.id;
+}
+
+fn get_last_sibling(process: *Process) *Process {
+    var current = process.next_sibling;
+    while(current) |c| {
+        current = c.next_sibling;
+    }
+    return current;
+}
