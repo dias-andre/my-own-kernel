8b2d600f7f589fe20ca4246ace61c2a5a59a2cb8
https-dre <diaso.andre@outlook.com>
2026-02-04 18:34:56 -0300

    refactor(vmm): simplify init function and enhance page allocation logic

--- kernel/mm/vmm.zig
+++ kernel/mm/vmm.zig
@@ -5,43 +5,44 @@ const log = @import("../utils/klog.zig").Logger;
 pub var is_paging_enabled: bool = false;
 pub var kernel_directory: usize = undefined;
 
-pub fn init(memory_size: usize) void {
+pub fn init() void {
+    log.info("(VMM) Initializing Virtual Memory Management", .{});
+    const memory_size = arch.memory.max_ram();
     const page_phys = pmm.allocate_page() orelse  {
         log.failed("Failed to allocate physical pages during VMM start", .{});
         while(true) arch.cpu.idle();
     };
-    arch.paging.clear_page_directory(page_phys);
+
+    arch.paging.init_page_directory(page_phys);
+    log.println("- Directory initialized!", .{});
+
+    log.debug("VMM: Allocated page directory at phys: {}", .{@as(*u64, @ptrFromInt(page_phys))});
+    log.debug("VMM: Is aligned? {}", .{page_phys % 4096});
+    if((page_phys % arch.memory.PAGE_SIZE) != 0) @panic("VMM PANIC: Page phys not aligned!");
 
     const aligned_limit = (memory_size + (arch.memory.PAGE_SIZE - 1)) & ~@as(usize, arch.memory.PAGE_SIZE - 1);
+    var current_addr: usize = 0;
+    const flags = Flags.DATA_KERNEL;
 
-    // while(current_addr < aligned_limit) : (current_addr += arch.memory.PAGE_SIZE) {
-    //     const flags = arch.paging.Flags.DATA_KERNEL;
-    //     arch.paging.map(page_phys, current_addr, current_addr, flags) catch {
-    //         log.failed("Failed to allocate physical page during VMM initialization", .{});
-    //         while(true) arch.cpu.idle();
-    //     };
+    while(current_addr < aligned_limit) : (current_addr += arch.memory.PAGE_SIZE) {    
+        arch.paging.map(page_phys, current_addr, current_addr, flags) catch {
+            log.failed("Failed to allocate physical page during VMM initialization", .{});
+            while(true) arch.cpu.idle();
+        };
 
-    //     const virt_offset = current_addr + arch.memory.MEMORY_OFFSET;
-    //     arch.paging.map(page_phys, virt_offset, current_addr, flags) catch {
-    //         log.failed("Failed to allocate physical page during VMM initialization", .{});
-    //         while(true) arch.cpu.idle();
-    //     };
-    // }
-
-    map_range(page_phys, 0, 0, aligned_limit, Flags.DATA_KERNEL) catch {
-        log.failed("Identity Map failed", .{});
-        while (true) arch.cpu.idle();
-    };
-
-    const hh_start = arch.memory.MEMORY_OFFSET;
-    map_range(page_phys, hh_start, 0, aligned_limit, Flags.DATA_KERNEL) catch {
-        log.failed("Identity & Higher Half mapped", .{});
-        while (true) arch.cpu.idle();
-    };
+        const virt_offset = current_addr + arch.memory.MEMORY_OFFSET;
+        arch.paging.map(page_phys, virt_offset, current_addr, flags) catch {
+            log.failed("Failed to allocate physical page during VMM initialization", .{});
+            while(true) arch.cpu.idle();
+        };
+    }
 
     log.println("- Identity & Higher Half mapped (limit: {}MB)", .{aligned_limit / 1024 / 1024});
     log.println("- Loading page directory...", .{});
+    kernel_directory = page_phys;
     arch.paging.load_page_directory(page_phys);
+    arch.paging.set_paging_enabled();
+    log.ok("(VMM) Virtual Memory Manager initialized successfully!", .{});
 }
 
 fn map_range(root: usize, virt_start: usize, phys_start: usize, size: usize, flags: u32) !void {
@@ -58,13 +59,13 @@ fn map_range(root: usize, virt_start: usize, phys_start: usize, size: usize, fla
 }
 
 pub const Flags = struct {
-    pub const READABLE: u32 = 1 << 0;
-    pub const WRITABLE: u32 = 1 << 1;
-    pub const EXECUTABLE: u32 = 1 << 2;
-    pub const USER: u32 = 1 << 3;
-    pub const MMIO: u32 = 1 << 4;
+    pub const READABLE: usize = 1 << 0;
+    pub const WRITABLE: usize = 1 << 1;
+    pub const EXECUTABLE: usize = 1 << 2;
+    pub const USER: usize = 1 << 3;
+    pub const MMIO: usize = 1 << 4;
 
     pub const CODE_USER = READABLE | EXECUTABLE | USER;
     pub const DATA_USER = READABLE | WRITABLE | USER;
-    pub const DATA_KERNEL = READABLE | WRITABLE;
+    pub const DATA_KERNEL = WRITABLE;
 };
