b6ebe76e273d9081ab7d64677209f65fa70faf55
https-dre <diaso.andre@outlook.com>
2026-02-04 20:39:54 -0300

    refactor(interface): remove obsolete driver interface definitions

--- kernel/drivers/interface.zig
+++
@@ -1,8 +0,0 @@
-pub const DriverType = enum { CHAR, BLOCK, NETWORK };
-
-pub const Driver = struct {
-  name: []const u8,
-  type: DriverType,
-  init_fn: *const fn() void,
-  read_fn: *const fn() u8,
-};
