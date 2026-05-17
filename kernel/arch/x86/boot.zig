9ec722f3bf61155103ba28ca0e0cb434a70c12c3
dias-andre <diaso.andre@outlook.com>
2026-05-17 20:10:05 -0300

    feat(kernel_boot): get control from UEFI

--- kernel/arch/x86/boot.zig
+++ kernel/arch/x86/boot.zig
@@ -1,17 +1,36 @@
+const uefi = @import("std").os.uefi;
 const multiboot = @import("multiboot.zig");
 const mem = @import("mem/memory.zig");
 const gdt = @import("cpu/gdt.zig");
 const idt = @import("interrupts/idt.zig");
 const cpu = @import("cpu/cpu.zig");
+const serial = @import("serial.zig");
 
 extern fn kernel_main() noreturn;
 
-export fn kernel_boot(pointer: u64, magic: u64) callconv(.c) noreturn {
-    const mb_info = multiboot.init(pointer, magic);
-    mem.init(mb_info);
+// export fn kernel_boot(pointer: u64, magic: u64) callconv(.c) noreturn {
+//     const mb_info = multiboot.init(pointer, magic);
+//     mem.init(mb_info);
+//     gdt.init();
+//     idt.init();
+//     cpu.enable_sse();
+//     kernel_main();
+//     unreachable;
+// }
+
+export fn kernel_boot(map: [*]uefi.tables.MemoryDescriptor, map_size: usize, desc_size: usize) linksection(".text.kernel_boot") callconv(.c) noreturn {
+    _ = map;
+    _ = map_size;
+    _ = desc_size;
+
+    serial.print("Reached kernel_boot\n");
     gdt.init();
     idt.init();
     cpu.enable_sse();
+    serial.print("Call kernel_main\n");
+    while (true) {
+        asm volatile ("nop");
+    }
     kernel_main();
     unreachable;
 }
