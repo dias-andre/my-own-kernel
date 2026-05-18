7591aa2e77576f2d047cad3f07bde350abf4c613
dias-andre <diaso.andre@outlook.com>
2026-05-18 18:27:57 -0300

    refactor: move @panic implementation to main.zig

--- kernel/main.zig
+++ kernel/main.zig
@@ -1,7 +1,4 @@
-comptime {
-    _ = stubs;
-    _ = arch.boot;
-}
+const std = @import("std");
 const arch = @import("arch");
 const klog = @import("klog");
 
@@ -10,13 +7,12 @@ const apic = @import("apic/root.zig");
 
 const sys_exit = @import("sys/sys_exit.zig").sys_exit;
 const log = klog.Logger;
-const stubs = @import("utils/libc_stubs.zig");
 
 extern var _start: u8;
 extern var _end: u8;
 
 export fn kernel_main() noreturn {
-    klog.init();
+    log.init();
     log.info("The execution reached kernel main", .{});
     mm.init(@intFromPtr(&_end));
 
@@ -42,11 +38,16 @@ fn map_video_address() void {
         log.failed("Failed to map physical address {} to virtual address {}", .{ vga_physical, vga_virtual });
         while (true) arch.cpu.idle();
     };
-    klog.screen.video_address = vga_virtual;
     log.ok("VGA mapped to {}", .{@as(*u64, @ptrFromInt(vga_virtual))});
 }
 
-fn testFaultHandler() void {
-    const bad_ptr: *u64 = @ptrFromInt(0xB0000000);
-    bad_ptr.* = 0xDEADBEEF;
+pub fn panic(msg: []const u8, _: ?*std.builtin.StackTrace, _: ?usize) noreturn {
+    // @setCold(true);
+    asm volatile ("cli");
+    log.failed("PANIC! {}", .{msg});
+    while (true) asm volatile ("hlt");
+}
+
+comptime {
+    _ = arch.boot;
 }
--- kernel/utils/libc_stubs.zig
+++ kernel/utils/libc_stubs.zig
@@ -1,6 +1,4 @@
-const arch = @import("arch");
 const std = @import("std");
-const log = @import("klog").Logger;
 
 export fn memcpy(dest: [*]u8, src: [*]const u8, n: usize) callconv(.c) ?[*]u8 {
     var i: usize = 0;
@@ -29,14 +27,4 @@ export fn memmove(dest: [*]u8, src: [*]const u8, n: usize) callconv(.c) ?[*]u8 {
         dest[i] = src[i];
     }
     return dest;
-}
-
-pub fn panic(msg: []const u8, _: ?*std.builtin.StackTrace, _: ?usize) noreturn {
-    // @setCold(true);
-    arch.cpu.disable_interrupts();
-
-    log.failed("!!! KERNEL PANIC !!!", .{});
-    log.println("{s}", .{msg});
-
-    while (true) arch.cpu.idle();
 }
