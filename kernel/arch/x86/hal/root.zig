ffbeb5748ae4420432d6f0fc9f36a0395d92e2b0
dias-andre <diaso.andre@outlook.com>
2026-05-21 17:55:46 -0300

    fix(hal) disable legacy PIC during HAL initialization
    - Move ArchCpuData to HAL root module.
    - Remove unused file "smp.zig"

--- kernel/arch/x86/hal/root.zig
+++ kernel/arch/x86/hal/root.zig
@@ -1,7 +1,19 @@
+const log = @import("klog").Logger;
 const acpi = @import("./acpi.zig");
 const apic = @import("./apic.zig");
+const cpu = @import("../cpu/cpu.zig");
+
+pub const ArchCpuData = struct {
+    apic_id: u32,
+    acpi_id: u32,
+};
 
 pub fn init_hardware(rsdp_addr: u64) void {
+    asm volatile ("cli");
+    cpu.outb(0x21, 0xFF);
+    cpu.outb(0xA1, 0xFF);
+    log.debug("Legacy PIC disabled!", .{});
     acpi.init(rsdp_addr);
     apic.map(acpi.get_madt_addr());
+    asm volatile ("sti");
 }
--- kernel/arch/x86/smp.zig
+++
@@ -1,4 +0,0 @@
-pub const ArchCpuData = struct {
-    apic_id: u32,
-    acpi_id: u32,
-};
