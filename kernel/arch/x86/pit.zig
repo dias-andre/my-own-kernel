b99cb14067bf3c6229662ec578fd6b22c751ea0d
dias-andre <diaso.andre@outlook.com>
2026-05-24 18:11:29 -0300

    feat(pit): add sleep_ms method

--- kernel/arch/x86/pit.zig
+++ kernel/arch/x86/pit.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const cpu = @import("cpu/cpu.zig");
 const pic = @import("interrupts/pic.zig");
 
@@ -5,10 +6,10 @@ const BASE_FREQUENCY = 1193180;
 const COMMAND_PORT = 0x43;
 const DATA_PORT_0 = 0x40;
 
-var ticks: u64 = 0;
-var handler: *const fn () void = undefined;
+var ticks = std.atomic.Value(u64).init(0);
+var timer_enabled: bool = false;
 
-pub fn init(freq: u32, func: *const fn () void) void {
+pub fn init(freq: u32) void {
     const divisor = BASE_FREQUENCY / freq;
     cpu.outb(COMMAND_PORT, 0x36);
 
@@ -17,13 +18,38 @@ pub fn init(freq: u32, func: *const fn () void) void {
 
     cpu.outb(DATA_PORT_0, l);
     cpu.outb(DATA_PORT_0, h);
-    handler = func;
+    timer_enabled = true;
+}
+
+pub fn disable() void {
+    asm volatile ("cli");
+    cpu.outb(0x21, 0xFF);
+    cpu.outb(0xA1, 0xFF);
+    asm volatile ("sti");
 }
 
 pub fn handle_irq() void {
-    ticks += 1;
+    _ = ticks.fetchAdd(1, .monotonic);
+    send_eoi();
 }
 
 pub fn get_ticks() u64 {
-    return ticks;
+    return ticks.load(.monotonic);
+}
+
+pub fn sleep_ms(ms: u32) void {
+    const start = ticks.load(.monotonic);
+    while (true) {
+        const now = ticks.load(.monotonic);
+        const elapsed = now -% start;
+
+        if (elapsed >= ms) {
+            break;
+        }
+        cpu.idle();
+    }
+}
+
+pub fn send_eoi() void {
+    pic.sendEOI(0);
 }
