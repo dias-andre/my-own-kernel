8b626ad4c18202d695a7fdc39b26b30ce5922e10
dias-andre <diaso.andre@outlook.com>
2026-05-26 16:29:00 -0300

    cleanup: remove unused IRQ hal interface

--- kernel/hal/irq.zig
+++
@@ -1,6 +0,0 @@
-pub const InterruptController = struct {
-    init: fn() void,
-    enable_irq: fn(irq_number: u32) void,
-    disable_irq: fn(irq_number: u32) void,
-    ack_irq: fn(irq_number: u32) void
-};
